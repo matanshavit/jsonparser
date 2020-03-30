b72bce939fe80f6820fbd554460b434164a17c4c
Matan Shavit <matan@apollo350.com>
2020-03-29 20:39:31 -0400

    create unshift method to remove first characters from remainder of json string

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -20,37 +20,37 @@ class Parser {
   }
 
   parseNextArray() {
-    this.remainder = this.remainder.slice(1)  // remove [
+    this.unshift(1)  // remove [
     var result = []
     var nextSeperator = this.remainder[0]
     if (nextSeperator === ']') {
-      this.remainder = this.remainder.slice(1) //remove ]
+      this.unshift(1) //remove ]
       return []
     }
     while (nextSeperator !== ']') {
       result.push(this.parseNextItem())
       nextSeperator = this.remainder[0]
-      this.remainder = this.remainder.slice(1)  // remove , or ]
+      this.unshift(1)  // remove , or ]
     }
     return result
   }
 
   parseNextObject() {
-    this.remainder = this.remainder.slice(1)  // remove {
+    this.unshift(1)  // remove {
     var result = {}
     var nextSeperator = ','
     while (nextSeperator !== '}') {
       var key = this.parseNextItem()
-      this.remainder = this.remainder.slice(1)  // remove :
+      this.unshift(1)  // remove :
       result[key] = this.parseNextItem()
       nextSeperator = this.remainder[0]
-      this.remainder = this.remainder.slice(1)  // remove , or }
+      this.unshift(1)  // remove , or }
     }
     return result
   }
 
   parseNextString() {
-    this.remainder = this.remainder.slice(1)  // remove "
+    this.unshift(1)  // remove "
     var result = ''
     while(this.remainder[0] !== '"') {
       if (
@@ -59,34 +59,38 @@ class Parser {
           this.remainder[1] === '"'
         )
       ) {
-        this.remainder = this.remainder.slice(1)
+        unshift(1)  // remove \
       }
       result = result + this.remainder[0]
-      this.remainder = this.remainder.slice(1)
+      this.unshift(1)  // remove \ or " (escaped)
     }
-    this.remainder = this.remainder.slice(1)  // remove "
+    this.unshift(1)  // remove "
     return result
   }
 
   parseNextInt() {
     const intString = this.remainder.match(/^\d+/)[0]
-    this.remainder = this.remainder.slice(intString.length)
+    this.unshift(intString.length)
     return parseInt(intString)
   }
 
   parseNextConstant() {
-    if (this.remainder.slice(0, 4) === 'true') {
-      this.remainder = this.remainder.slice(4)
+    if (this.remainder.startsWith('true')) {
+      this.unshift(4)
       return true
     }
-    if (this.remainder.slice(0, 5) === 'false') {
-      this.remainder = this.remainder.slice(5)
+    if (this.remainder.startsWith('false'))  {
+      this.unshift(5)
       return false
     }
-    if (this.remainder.slice(0, 4) === 'null') {
-      this.remainder = this.remainder.slice(4)
+    if (this.remainder.startsWith('null'))  {
+      this.unshift(4)
       return null
     }
+  }
+
+  unshift(length) {
+    this.remainder = this.remainder.slice(length)
   }
 
   parse() {
